a92459b4df4a3b2ac5e5e3d754edcca1dc59c169
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-08 19:29:43 +0700

    a

--- src/components/List.js
+++ src/components/List.js
@@ -10,9 +10,28 @@ const words = [
 export default class List extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+       <div>
+           {words.map(word => (
+                <div className="word">
+                    <div className="word-container">
+                    <h3 className="text-success">{word.en}</h3>
+                    <h3 className="text-danger">
+                        {word.isMemorized ? '----' : word.vn}
+                    </h3>
+                    </div>
+                    <div className="btn-container">
+                    <button 
+                        className={word.isMemorized ? 'btn btn-success' : 'btn btn-danger'}>
+                        {word.isMemorized ? 'Forgot' : 'Memorized'}
+                    </button>
+            
+                    <button className="btn btn-warning" >
+                        Remove
+                    </button>
+                    </div>
+                </div>
+           ))}
+       </div>
     )
   }
 }
