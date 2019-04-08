c8c36e78826b027e53cf25ed1ae3098b140cf93b
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-08 18:52:14 +0700

    a

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,15 +1,21 @@
 import React , {Component} from 'react';
 
 export default class Word extends Component{
+    getButton(){
+        if(this.props.wordInfo.isMemorized){
+           return <button className="btn btn-success">Forgot</button> 
+        }else{
+           return <button className="btn btn-danger">Memorized</button>
+        }     
+    }
     render(){
         const {en , vn , isMemorized} = this.props.wordInfo;
         const color = isMemorized ? 'green' : 'red';
         return(
-            <div>
+            <div style={{backgroundColor : 'gray' , margin : 10 , padding : 10 , borderRadius : 10 , width : 200}}>
                 <h3 style={{color}}>{en}</h3>
                 <p>{vn}</p>
-                <button className="btn btn-success">Forgot</button>            
-                <button className="btn btn-danger">Memorized</button>            
+                {this.getButton()}               
             </div>
         )
     }
