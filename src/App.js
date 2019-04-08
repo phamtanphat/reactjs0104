a9ba688fab4f97c4f5ce0a1b0a7d208e0fc6f149
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-08 18:39:39 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Words from './components/Word'
+import Word from './components/Word'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Words/>
+        <Word wordInfo={{en : 'One' , vn : 'Mot' , isMemorized : true}}/>
+        <Word wordInfo={{en : "Two", vn : "Hai" , isMemorized : false}}/>
+        <Word wordInfo={{en : "Three" , vn : "Ba" , isMemorized : true}}/>  
       </div>
     );
   }
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,8 +1,16 @@
 import React , {Component} from 'react';
+
 export default class Word extends Component{
     render(){
+        const {en , vn , isMemorized} = this.props.wordInfo;
+        const color = isMemorized ? 'green' : 'red';
         return(
-            <h3>Xin chao</h3>
+            <div>
+                <h3 style={{color}}>{en}</h3>
+                <p>{vn}</p>
+                <button className="btn btn-success">Forgot</button>            
+                <button className="btn btn-danger">Memorized</button>            
+            </div>
         )
     }
 }
