ac997eeaa1cddf4d4ef3de6120cdbb03fad40c37
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-10 18:14:12 +0700

    a

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Box extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       value : 0
+    }
+  }
   render() {
     return (
       <div>
-        <h3>Value = 0</h3>
-        <button className="btn btn-success">Increase</button>
-        <button className="btn btn-warning">Descrease</button>
-        <button className="btn btn-danger">Reset</button>
+        <h3>Value = {this.state.value}</h3>
+        <button 
+          onClick={() => this.setState({value : this.state.value + 1}) }
+          className="btn btn-success">
+          Increase
+        </button>
+        <button 
+          onClick={() =>  this.setState({value : this.state.value - 1}) }
+          className="btn btn-warning">
+          Descrease
+        </button>
+        <button 
+          onClick={() =>  this.setState({value : 0}) }
+          className="btn btn-danger">
+          Reset
+        </button>
       </div>
     )
   }
