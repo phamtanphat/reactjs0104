6d896eeb3a80dfdf939e0c0e614bcc672517d7bf
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-10 18:55:12 +0700

    a

--- src/components/List.js
+++ src/components/List.js
@@ -18,29 +18,32 @@ export default class List extends Component {
     });
     this.setState({words : newWords});
   }
+  getWordItem(word){
+      return (
+        <div className="word" key={word.id}>
+            <div className="word-container">
+            <h3 className="text-success">{word.en}</h3>
+            <h3 className="text-danger">
+                {word.isMemorized ? '----' : word.vn}
+            </h3>
+            </div>
+            <div className="btn-container">
+            <button
+                onClick={() =>this.togglWord(word.id)} 
+                className={word.isMemorized ? 'btn btn-success' : 'btn btn-danger'}>
+                {word.isMemorized ? 'Forgot' : 'Memorized'}
+            </button>
+            <button className="btn btn-warning" >
+                Remove
+            </button>
+            </div>
+        </div>
+      )
+  }
   render() {
     return (
        <div>
-           {this.state.words.map(word => (
-                <div className="word" key={word.id}>
-                    <div className="word-container">
-                    <h3 className="text-success">{word.en}</h3>
-                    <h3 className="text-danger">
-                        {word.isMemorized ? '----' : word.vn}
-                    </h3>
-                    </div>
-                    <div className="btn-container">
-                    <button
-                        onClick={() =>this.togglWord(word.id)} 
-                        className={word.isMemorized ? 'btn btn-success' : 'btn btn-danger'}>
-                        {word.isMemorized ? 'Forgot' : 'Memorized'}
-                    </button>
-                    <button className="btn btn-warning" >
-                        Remove
-                    </button>
-                    </div>
-                </div>
-           ))}
+           {this.state.words.map(word => this.getWordItem(word))}
        </div>
     )
   }
