a81e0d70113456da2612938cc9100a00d717112c
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-19 18:04:04 +0700

    a

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ const store = createStore((state = defaultState , action) =>{
 })
 //https://bit.ly/2Gd8oHq
 
+// 1 : Định nghĩa ra store =>
+// state :  giá trị state đưa vào store chính dữ liệu sẽ truyền qua components
+// action : 
+    // + tín hiệu : type (Nhìn vào tín hiệu để thay đổi store)
+    // + tham số truyền vào (action.tenthuoctinh) : giá trị này sẽ được nằm trong action
+// mặc đinh phải return về state
+
+//2 : Truyền store vào trong components (sử dụng Provider)
+// 3 : Kết nối với components thông connect
+// 4 : Thay đổi store thông qua dispatch
 class App extends Component {
   render() {
     return (
