608afc83a055c1828dc0c8cce5e2621777f54e55
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-19 19:15:57 +0700

    a

--- src/App.js
+++ src/App.js
@@ -2,40 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 // import Box from './components/Box';
 import List from './components/List';
-import { createStore } from 'redux'
+import store from './components/redux/store';
 import {Provider} from 'react-redux';
 
-const defaultState = {
-  words : [
-    {id : 'a1' , en : 'One' , vn : 'Mot' , isMemorized : true},
-    {id : 'a2' , en : 'Two' , vn : 'Hai' , isMemorized : false},
-    {id : 'a3' , en : 'Three' , vn : 'Ba' , isMemorized : false}
-  ],
-  shouldShowForm : false,
-  filterMode : 'Show_All'
-}
 
-const store = createStore((state = defaultState , action) =>{
-  if(action.type === "REMOVE_WORD"){
-    const newWords = state.words.filter(w => w.id !== action.id);
-    return {...state,words : newWords}
-  }
-  if(action.type === 'TOGGLE_WORD'){
-    const newWords = state.words.map(w =>{
-        if(w.id !== action.id) return w;
-        return {...w, isMemorized : !w.isMemorized}
-    });
-    return {...state , words :newWords}
-  }
-  if(action.type === 'SET_FILTER_MODE') return {...state , filterMode : action.filterMode}
-  if(action.type === 'ADD_WORD'){
-     const newWords = state.words.concat(action.word);
-     return {...state,words : newWords , shouldShowForm : false}
-  }
-  if(action.type === 'TOGGLE_FORM') return {...state , shouldShowForm : !state.shouldShowForm}
-  
-  return state;
-})
 
 //https://bit.ly/2Gd8oHq
 
---
+++ src/components/redux/store.js
@@ -0,0 +1,34 @@
+import { createStore } from 'redux'
+const defaultState = {
+    words : [
+      {id : 'a1' , en : 'One' , vn : 'Mot' , isMemorized : true},
+      {id : 'a2' , en : 'Two' , vn : 'Hai' , isMemorized : false},
+      {id : 'a3' , en : 'Three' , vn : 'Ba' , isMemorized : false}
+    ],
+    shouldShowForm : false,
+    filterMode : 'Show_All'
+  }
+  
+const store = createStore((state = defaultState , action) =>{
+    if(action.type === "REMOVE_WORD"){
+      const newWords = state.words.filter(w => w.id !== action.id);
+      return {...state,words : newWords}
+    }
+    if(action.type === 'TOGGLE_WORD'){
+      const newWords = state.words.map(w =>{
+          if(w.id !== action.id) return w;
+          return {...w, isMemorized : !w.isMemorized}
+      });
+      return {...state , words :newWords}
+    }
+    if(action.type === 'SET_FILTER_MODE') return {...state , filterMode : action.filterMode}
+    if(action.type === 'ADD_WORD'){
+       let newWords = state.words.concat(action.word);
+       return {...state,words : newWords , shouldShowForm : false}
+    }
+    if(action.type === 'TOGGLE_FORM') return {...state , shouldShowForm : !state.shouldShowForm}
+    
+    return state;
+  });
+
+export default store;
